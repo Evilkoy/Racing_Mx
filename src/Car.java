a5b0e9c5c436eee6ebb5cf030eb27fb2c4204a47
Evilkoy <drobkill@gmail.com>
2022-08-03 20:02:13 +0300

    second fix cars

--- src/Car.java
+++ src/Car.java
@@ -1,40 +1,33 @@
 import java.util.HashMap;
 
 public class Car {
-    HashMap<> cars = new HashMap<>();
 
-    private static String model;
-    private static int carCost;
-    private static int rating;
+    private String model;
+    private int carCost;
+    private int rating;
 
     public Car(String model, int carCost,int rating) {
-        Car.model = model;
-        Car.carCost = carCost;
-        Car.rating = rating;
+        this.model = model;
+        this.carCost = carCost;
+        this.rating = rating;
     }
-    public Car getFord(){
-        Car ford = new Car("Ford Mustang",10200,50);
-        return ford;
+    public static Car getFord(){
+        return new Car("Ford Mustang",10200,50);
     }
-    public Car getBMW(){
-        Car bmw = new Car("BMW M3",12000,60);
-        return bmw;
+    public static Car getBMW(){
+        return new Car("BMW M3",12000,60);
     }
-    public Car getChevrolet(){
-        Car chevrolet = new Car("Chevrolet Cobalt SS",9500,40);
-        return chevrolet;
+    public static Car getChevrolet(){
+        return new Car("Chevrolet Cobalt SS",9500,40);
     }
-    public Car getSubaru(){
-        Car subaru = new Car("Subaru Impreza",13500,70);
-        return subaru;
+    public static Car getSubaru(){
+        return new Car("Subaru Impreza",13500,70);
     }
-    public Car getAudi(){
-        Car audi = new Car("Audi RS", 14000,80);
-        return audi;
+    public static Car getAudi(){
+        return new Car("Audi RS", 14000,80);
     }
-    public Car getNissan(){
-        Car nissan = new Car("Nissan GTR", 15000,90);
-        return nissan;
+    public static Car getNissan(){
+        return new Car("Nissan GTR", 15000,90);
     }
 
 }
