2aa14f70987874b106b0139e53083ab74c38b978
Evilkoy <drobkill@gmail.com>
2022-08-04 00:29:42 +0300

    fixed randomcars

--- src/RandomCars.java
+++ src/RandomCars.java
@@ -1,31 +1,27 @@
 import java.util.HashMap;
 import java.util.HashSet;
 public class RandomCars {
-    private HashMap<Integer,Car> carMap = new HashMap<>();
+    private HashMap<Integer,Car> carMap = new HashMap<>(){{
+        put(0,Car.getAudi());
+        put(1,Car.getBMW());
+        put(2,Car.getChevrolet());
+        put(3,Car.getFord());
+        put(4,Car.getNissan());
+        put(5,Car.getSubaru());
+    }};
     private HashSet<Car> carSet = new HashSet<>();
-
-    public HashSet<Car> getCarSet() {
-        return carSet;
-    }
 
     public HashMap<Integer, Car> getCarMap() {
         return carMap;
     }
 
-    public void setCarMap(){
-        carMap.put(0,Car.getAudi());
-        carMap.put(1,Car.getBMW());
-        carMap.put(2,Car.getChevrolet());
-        carMap.put(3,Car.getFord());
-        carMap.put(4,Car.getNissan());
-        carMap.put(5,Car.getSubaru());
-    }
-    public void setCarSet(){
+    public HashSet<Car> getStartedCars(){
         while (carSet.size()<3){
             carSet.add(carMap.get(randomSix()));
         }
+        return carSet;
     }
-    static int randomSix(){
-        return (int)(Math.random()*7);
+    int randomSix(){
+        return (int)(Math.random()*6);
     }
 }
