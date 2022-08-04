c1fdab748881560c7e96df34c2254653602652c0
Evilkoy <drobkill@gmail.com>
2022-08-05 01:28:39 +0300

    added shop, map moved

---
+++ src/Shop.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Shop {
+    private HashMap<String, Upgrade> upgrades = new HashMap<>() {{
+        put("engine", new Upgrade("Engine", 0, 3000));
+        put("gear", new Upgrade("Gear", 0, 3000));
+        put("tyres", new Upgrade("Tyres", 0, 3000));
+        put("turbo", new Upgrade("Turbo", 0, 3000));
+        put("body", new Upgrade("Body", 0, 3000));
+    }};
+    public Upgrade getUpgrade(String name){
+        return upgrades.get(name);
+    }
+}
---
+++ src/Upgrade.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+public class Upgrade {
+    private int tier;
+    private int cost;
+    private String type;
+
+    public void setTier(int tier) {
+        this.tier = tier;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Upgrade(String type, int tier, int cost) {
+        this.type = type;
+        this.tier = tier;
+        this.cost = cost;
+    }
+
+    public Upgrade() {
+    }
+    public int getRating(Upgrade upgrade) {
+        return 10 * (2 * upgrade.tier);
+    }
+}
