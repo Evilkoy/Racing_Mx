04f1eadeeb4d935e7054629f84fc90fc0f8d9361
Evilkoy <drobkill@gmail.com>
2022-08-10 16:58:31 +0300

    method moved in new class

--- src/StartScreen.java
+++ src/StartScreen.java
@@ -18,21 +18,9 @@ public class StartScreen {
     private Player carChoice(Player player) {
         int i = InOutService.inputInt();
         if (0 < i && i < 4) {
-            return purchaseStartCar(player,startCars.get(i));
+            return MoneyService.purchaseStartCar(player,startCars.get(i));
         } else {
             System.out.println("Wrong choice, try again");
-            return carChoice(player);
-        }
-    }
-
-    public Player purchaseStartCar(Player player, Car car) {
-        if (player.getMoney() >= car.getCarCost()){
-            player.setMoney(player.getMoney() - car.getCarCost());
-            player.setMyCar(car);
-            System.out.println("Your new car is "+car.getModel());
-            return player;
-        }else{
-            System.out.println("Not enough money, make another choice");
             return carChoice(player);
         }
     }
